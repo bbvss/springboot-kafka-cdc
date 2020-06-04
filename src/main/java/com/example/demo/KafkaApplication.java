682a70addfd1606982cd735143cb5bdbb765f4f6
clemens.kunwald <clemens.kunwald@bbv.ch>
2020-06-04 18:01:41 +0200

    main application

--- src/main/java/com/example/demo/KafkaApplication.java
+++ src/main/java/com/example/demo/KafkaApplication.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -9,8 +7,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 @SpringBootApplication
 @EnableKafka
 public class KafkaApplication {
-
-  private static final Logger logger = LoggerFactory.getLogger(KafkaApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(KafkaApplication.class, args);
